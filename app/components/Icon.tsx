9fcb7282cb858cad5397ad8c039b8eb102bc8a8d
merenkirkasai <144036220+CoinMarketJob@users.noreply.github.com>
2024-07-10 01:01:59 +0300

    Update Front end

--- app/components/Icon.tsx
+++ app/components/Icon.tsx
@@ -6,11 +6,12 @@ interface IconProps {
     children: React.ReactNode;
     size : number;
     margin? : boolean
+    marginLeft? : boolean
   }
 
-const Icon:React.FC<IconProps> = ({ onClick, children, size, margin }) => {
+const Icon:React.FC<IconProps> = ({ onClick, children, size, margin, marginLeft }) => {
   return (
-    <div className="icon-div" onClick={onClick} style={{marginLeft: margin ? '1rem' : '0rem', position: "relative" }}>
+    <div className="icon-div" onClick={onClick} style={{marginLeft: margin ? '1rem' : marginLeft ? "2.7px" : '0rem', position: "relative" }}>
       <div className="icon-hover" style={{width: size, height: size}}>
         {children}
       </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet' />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet" />
         <link rel="shortcut icon" href="/favicon.ico" />
       </head>
       <body>
