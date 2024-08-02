030896474d8d4694b036604be134938032673f04
merenkirkasai <144036220+CoinMarketJob@users.noreply.github.com>
2024-08-03 00:24:51 +0300

    Update

--- app/api/live/get/route.ts
+++ app/api/live/get/route.ts
@@ -2,14 +2,9 @@
 import prisma from "@/libs/prismadb";
 import { NextResponse } from "next/server";
 
-export async function POST(request: Request) {
-  const body = await request.json();
-  const {
-    email
-  } = body;
-
+export async function GET() {
   const user = await prisma.user.findUnique({
-    where: { email }
+    where: { email: "merenkirkas@gmail.com" }
     });
 
 
