1ccdb4a7575c6e018f08531d4e9b8a4b3942c33c
merenkirkasai <144036220+CoinMarketJob@users.noreply.github.com>
2024-08-03 20:53:05 +0300

    update

--- app/api/job/get/route.ts
+++ app/api/job/get/route.ts
@@ -4,7 +4,13 @@ import prisma from "@/libs/prismadb";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    const jobs = await prisma.job.findMany();
-
-    return NextResponse.json(jobs);
+    try {
+        const jobs = await prisma.job.findMany();
+    
+        return NextResponse.json(jobs);
+    }
+    catch (e) {
+        console.error(e);
+        return NextResponse.json({ error: 'Server Error' }, { status: 404 })
+    }
 }
