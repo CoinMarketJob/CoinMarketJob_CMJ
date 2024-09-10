73bae1583ab6cbdfaa0b9d4faf0e6d938ecaea50
CoinMarketJob <144036220+CoinMarketJob@users.noreply.github.com>
2024-09-10 05:54:28 +0300

    Filter hover

--- app/components/Search/SearchInput.tsx
+++ app/components/Search/SearchInput.tsx
@@ -8,6 +8,7 @@ import { useJobs } from '@/hooks/useJobs';
 import { useRouter } from 'next/navigation';
 import JobFilterPopUp from '../jobfilter/Job';
 import { Job as PrismaJob, JobType, LocationType, ExperienceLevel } from '@prisma/client';
+import Icon from '../general/Icon';
 
 // Update the Job interface to match the Prisma Job type
 interface Job extends Omit<PrismaJob, 'date'> {
@@ -151,17 +152,24 @@ const SearchInput: React.FC<SearchProps> = ({ tags, setTags, isFilterOpen, setIs
                 onBlur={handleBlur}
                 ref={inputRef}
             />
-            <div className="filter-div" onClick={toggleFilter}>
-            <svg width="28" height="35" viewBox="0 0 37 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M0 5C0 4.44772 0.447715 4 1 4H8V6H1C0.447715 6 0 5.55228 0 5Z" fill="#242220" fill-opacity="0.4"/>
-                <path d="M37 5C37 5.55228 36.5523 6 36 6L16 6V5V4L36 4C36.5523 4 37 4.44772 37 5Z" fill="#242220" fill-opacity="0.4"/>
-                <path d="M17 5C17 7.76142 14.7614 10 12 10C9.23858 10 7 7.76142 7 5C7 2.23858 9.23858 0 12 0C14.7614 0 17 2.23858 17 5Z" fill="#A7A7A6"/>
-                <path d="M37 19C37 19.5523 36.5523 20 36 20H29V18H36C36.5523 18 37 18.4477 37 19Z" fill="#242220" fill-opacity="0.4"/>
-                <path d="M0 19C0 18.4477 0.447715 18 1 18L21 18V19V20L1 20C0.447715 20 0 19.5523 0 19Z" fill="#242220" fill-opacity="0.4"/>
-                <path d="M20 19C20 16.2386 22.2386 14 25 14C27.7614 14 30 16.2386 30 19C30 21.7614 27.7614 24 25 24C22.2386 24 20 21.7614 20 19Z" fill="#A7A7A6"/>
-                <path d="M22 19C22 17.3431 23.3431 16 25 16C26.6569 16 28 17.3431 28 19C28 20.6569 26.6569 22 25 22C23.3431 22 22 20.6569 22 19Z" fill="white"/>
-                <path d="M9 5C9 3.34315 10.3431 2 12 2C13.6569 2 15 3.34315 15 5C15 6.65685 13.6569 8 12 8C10.3431 8 9 6.65685 9 5Z" fill="white"/>
-            </svg>
+            <div className="filter-icon-container">
+                <Icon
+                    onClick={toggleFilter}
+                    hoverSize={45}
+                    hoverContent="Filter"
+                    width={28}
+                >
+                    <svg width="28" height="35" viewBox="0 0 37 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 5C0 4.44772 0.447715 4 1 4H8V6H1C0.447715 6 0 5.55228 0 5Z" fill="#242220" fill-opacity="0.4"/>
+                        <path d="M37 5C37 5.55228 36.5523 6 36 6L16 6V5V4L36 4C36.5523 4 37 4.44772 37 5Z" fill="#242220" fill-opacity="0.4"/>
+                        <path d="M17 5C17 7.76142 14.7614 10 12 10C9.23858 10 7 7.76142 7 5C7 2.23858 9.23858 0 12 0C14.7614 0 17 2.23858 17 5Z" fill="#A7A7A6"/>
+                        <path d="M37 19C37 19.5523 36.5523 20 36 20H29V18H36C36.5523 18 37 18.4477 37 19Z" fill="#242220" fill-opacity="0.4"/>
+                        <path d="M0 19C0 18.4477 0.447715 18 1 18L21 18V19V20L1 20C0.447715 20 0 19.5523 0 19Z" fill="#242220" fill-opacity="0.4"/>
+                        <path d="M20 19C20 16.2386 22.2386 14 25 14C27.7614 14 30 16.2386 30 19C30 21.7614 27.7614 24 25 24C22.2386 24 20 21.7614 20 19Z" fill="#A7A7A6"/>
+                        <path d="M22 19C22 17.3431 23.3431 16 25 16C26.6569 16 28 17.3431 28 19C28 20.6569 26.6569 22 25 22C23.3431 22 22 20.6569 22 19Z" fill="white"/>
+                        <path d="M9 5C9 3.34315 10.3431 2 12 2C13.6569 2 15 3.34315 15 5C15 6.65685 13.6569 8 12 8C10.3431 8 9 6.65685 9 5Z" fill="white"/>
+                    </svg>
+                </Icon>
             </div>
 
             <div className='JobFilterPopUp'>
