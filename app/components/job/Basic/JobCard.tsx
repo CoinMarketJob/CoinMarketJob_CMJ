ed926924ce7379a8806814ee387467449d4fecdb
CoinMarketJob <144036220+CoinMarketJob@users.noreply.github.com>
2024-09-12 19:07:28 +0300

    Revert "Job"
    This reverts commit 277486ec2bca9c3d2eed5d28c2614d221ccb5539.

--- app/components/job/Basic/JobCard.tsx
+++ app/components/job/Basic/JobCard.tsx
@@ -35,7 +35,6 @@ const JobCard: React.FC<JobCardProps> = ({
   const { filteredJobs, setFilteredJobs } = useJobs();
   const id = job.id;
   const cardType = "left";
-  const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
   const [{ isDragging }, drag] = useDrag(
     () => ({
@@ -80,15 +79,7 @@ const JobCard: React.FC<JobCardProps> = ({
       }
     } catch (error) {}
   };
-  const JobShare = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    e.stopPropagation();
-    const shareUrl = `localhost:3000/jobs/view/${job.id}`;
-    navigator.clipboard.writeText(shareUrl);
-    setSuccessMessage("Link Copied to Clipboard");
-    setTimeout(() => {
-      setSuccessMessage(null);
-    }, 3000);
-  };
+
   const JobSelect = () => {
     onSelect(job.id);
     onClick(job);
@@ -112,9 +103,6 @@ const JobCard: React.FC<JobCardProps> = ({
       onClick={JobSelect}
       key={job.id}
     >
-      {successMessage && (
-        <div className={styles.SuccessMessage}>{successMessage}</div>
-      )}
       <div className={styles.icon}>
         <img
           src={job.logo || "/default-logo.png"}
@@ -132,50 +120,6 @@ const JobCard: React.FC<JobCardProps> = ({
         </p>
       </div>
       <div className={styles.actions}>
-        <div>
-          <Icon onClick={JobShare} hoverSize={45} hoverContent="Share">
-            <svg
-              width="18"
-              height="29"
-              viewBox="0 0 27 29"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M17.1484 24.5C17.1484 22.0147 19.1632 20 21.6484 20C24.1337 20 26.1484 22.0147 26.1484 24.5C26.1484 26.9853 24.1337 29 21.6484 29C19.1632 29 17.1484 26.9853 17.1484 24.5Z"
-                fill="#A7A7A6"
-              />
-              <path
-                d="M18.1484 24.5C18.1484 22.567 19.7154 21 21.6484 21C23.5814 21 25.1484 22.567 25.1484 24.5C25.1484 26.433 23.5814 28 21.6484 28C19.7154 28 18.1484 26.433 18.1484 24.5Z"
-                fill="white"
-              />
-              <path
-                d="M0 14.5C0 12.0147 2.01472 10 4.5 10C6.98528 10 9 12.0147 9 14.5C9 16.9853 6.98528 19 4.5 19C2.01472 19 0 16.9853 0 14.5Z"
-                fill="#A7A7A6"
-              />
-              <path
-                d="M1 14.5C1 12.567 2.567 11 4.5 11C6.433 11 8 12.567 8 14.5C8 16.433 6.433 18 4.5 18C2.567 18 1 16.433 1 14.5Z"
-                fill="white"
-              />
-              <path
-                d="M17.1484 4.5C17.1484 2.01472 19.1632 0 21.6484 0C24.1337 0 26.1484 2.01472 26.1484 4.5C26.1484 6.98528 24.1337 9 21.6484 9C19.1632 9 17.1484 6.98528 17.1484 4.5Z"
-                fill="#A7A7A6"
-              />
-              <path
-                d="M18.1484 4.5C18.1484 2.567 19.7154 1 21.6484 1C23.5814 1 25.1484 2.567 25.1484 4.5C25.1484 6.433 23.5814 8 21.6484 8C19.7154 8 18.1484 6.433 18.1484 4.5Z"
-                fill="white"
-              />
-              <path
-                d="M7.64844 12.093L18.0022 6.04275L18.5068 6.90615L8.15297 12.9564L7.64844 12.093Z"
-                fill="#A7A7A6"
-              />
-              <path
-                d="M18.006 22.8531L7.61021 16.8753L8.1087 16.0084L18.5045 21.9863L18.006 22.8531Z"
-                fill="#A7A7A6"
-              />
-            </svg>
-          </Icon>
-        </div>
         <div>
           <Icon onClick={JobSave} hoverSize={45} hoverContent="Save">
             <svg
