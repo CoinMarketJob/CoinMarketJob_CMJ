34b43031668f9954443d7056d46a794f2ea8ce68
CoinMarketJob <144036220+CoinMarketJob@users.noreply.github.com>
2024-09-13 10:45:42 +0300

    Descending order list

--- app/api/job/get/route.ts
+++ app/api/job/get/route.ts
@@ -6,12 +6,15 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try {
     const jobs = await prisma.job.findMany({
+      orderBy: {
+        id: 'desc'  // Bu satır işleri ID'ye göre azalan sırada sıralayacak
+      },
       cacheStrategy: { swr: 60, ttl: 60 }
     });
 
     return NextResponse.json(jobs);
-  } catch (e) {
-    console.error(e);
-    return NextResponse.json({ error: "Server Error" }, { status: 404 });
+  } catch (error) {
+    console.error("Error fetching jobs:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
