8bc0309c72e809c7d51534bac841e72200b97add
CoinMarketJob <coinmarketjob@gmail.com>
2024-09-26 23:27:35 +0300

    update
    +job detail bugs fixed
    +styles improved

--- app/components/job/JobDetails.tsx
+++ app/components/job/JobDetails.tsx
@@ -45,7 +45,7 @@ const JobDetails: React.FC<JobDetailsProps> = ({ job, onClose = () => {} }) => {
             {job && (
                 <div className={styles.headerJobDetails}>
                     <div className={styles.closeIcon} onClick={handleClose}>
-                        <Icon onClick={handleClose} hoverSize={14} hoverContent="Close">
+                        <Icon onClick={handleClose} hoverSize={45} hoverContent="Close">
                             <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M14 1.41L12.59 0L7 5.59L1.41 0L0 1.41L5.59 7L0 12.59L1.41 14L7 8.41L12.59 14L14 12.59L8.41 7L14 1.41Z" fill="#242220" fillOpacity="0.2"/>
                             </svg>
