ec6c766dec6c7269b4ca6220c50675a3b8ee5cff
boyraz-cmj <y.emre.boyraz0@gmail.com>
2024-10-22 18:47:26 +0300

    Revert "Update"
    This reverts commit 020ead396aa014524d4350eca7d5d31ed6864cc0.

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,21 +1,8 @@
-'use client'
-import React, { useState } from 'react'
-import Button from '../components/general/Button'
-import EditProfile from '../components/profile/EditProfile';
+import React from 'react'
 
 const page = () => {
-  const [editProfile, setEditProfile] = useState<boolean>(false);
-
-  const handleCloseEditProfile = () => {
-    setEditProfile(false);
-  };
-
   return (
-    <div className="tailwind">
-      <Button text="Edit Profile" onClick={() => setEditProfile(true)} />
-
-      {editProfile && <EditProfile onClose={handleCloseEditProfile} />}
-    </div>
+    <div>Profile</div>
   )
 }
 
