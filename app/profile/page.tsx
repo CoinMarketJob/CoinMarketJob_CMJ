020ead396aa014524d4350eca7d5d31ed6864cc0
eren-cmj <merenkirkas@protonmail.com>
2024-10-22 17:48:07 +0300

    Update

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import Button from '../components/general/Button'
+import EditProfile from '../components/profile/EditProfile';
 
 const page = () => {
+  const [editProfile, setEditProfile] = useState<boolean>(false);
+
+  const handleCloseEditProfile = () => {
+    setEditProfile(false);
+  };
+
   return (
-    <div>Profile</div>
+    <div className="tailwind">
+      <Button text="Edit Profile" onClick={() => setEditProfile(true)} />
+
+      {editProfile && <EditProfile onClose={handleCloseEditProfile} />}
+    </div>
   )
 }
 
